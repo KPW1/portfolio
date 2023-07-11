cbef1825f6d7db6368cbfb308dcb5ac33d9a3521
KPW1 <oooppp24@naver.com>
2023-07-11 14:27:51 +0900

    수정

--- index.js
+++ index.js
@@ -87,6 +87,27 @@ $(".btn4").on("click", (e) => {
     behavior: "smooth",
   });
 });
+const body = document.querySelector("body");
+// $(window).on("scroll", () => {
+//   const footer = $(".profile").offset().top - 500;
+//   console.log(footer);
+//   console.log(scrollY);
+//   if (scrollY > 100) {
+//     $("body").addClass("active");
+//   } else {
+//     $("body").removeClass("active");
+//   }
+// });
+const profile = document.querySelector(".skills");
+addEventListener("scroll", () => {
+  const center = profile.offsetTop;
+  console.log(center);
+  if (scrollY > center) {
+    document.body.classList.add("active");
+  } else {
+    document.body.classList.remove("active");
+  }
+});
 
 // locomotive
 // (function () {
