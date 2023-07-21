f8d7de20e4c7de53bb7be5ee9a85d7d16513cf46
KPW1 <oooppp24@naver.com>
2023-07-21 17:07:20 +0900

    12

--- project2/index.js
+++ project2/index.js
@@ -1,3 +1,26 @@
+const main_img = document.querySelectorAll('.page1 .img-wrapper img')
+
+
+$(document).ready(function() {
+  main_img.forEach((a) => {
+    a.style.transform = `scale(100%)`;
+  });
+  $('header').addClass('open');
+  $('.page1').find('.text-wrapper').find('.first').addClass('active')
+  $('.page1').find('.text-wrapper').find('.first-1').addClass('active')
+  $('.page1').find('.text-wrapper').find('.first-2').addClass('active')
+  $('.page1').find('.text-wrapper').find('.second').addClass('active')
+  $('.page1').find('.line').addClass('active');
+  setTimeout(() => {
+    $('.page1').find('.img-box').addClass('active')
+  }, 1000);
+  setTimeout(() => {
+    $('.img-box').find('img').addClass('active')
+  }, 2000);
+})
+
+
+
 $(window).on('scroll', () => {
   if(scrollY > 50) {
     $('header').addClass('active');
@@ -24,11 +47,11 @@ $(window).on('scroll', () => {
     tcard.forEach((a,idx,arr) => {
       arr[0].style.transform = `scale(100%)`;
     });
-  }else if((scrollY > 850) && (scrollY < 949)) {
+  }else if((scrollY > 850) && (scrollY < 1049)) {
     tcard.forEach((a,idx,arr) => {
       arr[1].style.transform = `scale(100%)`;
     });
-  }else if((scrollY > 950) && (scrollY < 1050)) {
+  }else if((scrollY > 950) && (scrollY < 1150)) {
     tcard.forEach((a,idx,arr) => {
       arr[2].style.transform = `scale(100%)`;
     });
@@ -64,7 +87,7 @@ $(window).on('scroll', () => {
       el: ".swiper-pagination",
       clickable: true,
       renderBullet: function (index, className) {
-        return '<span class="' + className + '">' + (bullet1[index]) + "</span>";
+        return '<span class="' + className  +   ' ' + className+index  +   '" >' +  (bullet1[index]) + "</span>";
       },
     },
     navigation: {
@@ -72,7 +95,12 @@ $(window).on('scroll', () => {
       prevEl: '.swiper-button-prev',
     },
   });
-
+  $(window).on('scroll', () => {
+    if(scrollY>1600) {
+      $('.swiper-pagination-bullet1').addClass('active')
+      $('.swiper-pagination-bullet2').addClass('active')
+    }
+  });
 
 !function(e,n){"function"==typeof define&&define.amd?define(["ScrollMagic","TweenMax","TimelineMax"],n):"object"==typeof exports?(require("gsap"),n(require("scrollmagic"),TweenMax,TimelineMax)):n(e.ScrollMagic||e.jQuery&&e.jQuery.ScrollMagic,e.TweenMax||e.TweenLite,e.TimelineMax||e.TimelineLite)}(this,function(e,n,o){"use strict";var t=window.console||{},r=Function.prototype.bind.call(t.error||t.log||function(){},t);e||r("(animation.gsap) -> ERROR: The ScrollMagic main module could not be found. Please make sure it's loaded before this plugin or use an asynchronous loader like requirejs."),n||r("(animation.gsap) -> ERROR: TweenLite or TweenMax could not be found. Please make sure GSAP is loaded before ScrollMagic or use an asynchronous loader like requirejs."),e.Scene.addOption("tweenChanges",!1,function(e){return!!e}),e.Scene.extend(function(){var e,t=this,r=function(){t._log&&(Array.prototype.splice.call(arguments,1,0,"(animation.gsap)","->"),t._log.apply(this,arguments))};t.on("progress.plugin_gsap",function(){i()}),t.on("destroy.plugin_gsap",function(e){t.removeTween(e.reset)});var i=function(){if(e){var n=t.progress(),o=t.state();e.repeat&&-1===e.repeat()?"DURING"===o&&e.paused()?e.play():"DURING"===o||e.paused()||e.pause():n!=e.progress()&&(0===t.duration()?n>0?e.play():e.reverse():t.tweenChanges()&&e.tweenTo?e.tweenTo(n*e.duration()):e.progress(n).pause())}};t.setTween=function(a,s,l){var c;arguments.length>1&&(arguments.length<3&&(l=s,s=1),a=n.to(a,s,l));try{(c=o?new o({smoothChildTiming:!0}).add(a):a).pause()}catch(e){return r(1,"ERROR calling method 'setTween()': Supplied argument is not a valid TweenObject"),t}if(e&&t.removeTween(),e=c,a.repeat&&-1===a.repeat()&&(e.repeat(-1),e.yoyo(a.yoyo())),t.tweenChanges()&&!e.tweenTo&&r(2,"WARNING: tweenChanges will only work if the TimelineMax object is available for ScrollMagic."),e&&t.controller()&&t.triggerElement()&&t.loglevel()>=2){var u=n.getTweensOf(t.triggerElement()),p=t.controller().info("vertical");u.forEach(function(e,n){var o=e.vars.css||e.vars;if(p?void 0!==o.top||void 0!==o.bottom:void 0!==o.left||void 0!==o.right)return r(2,"WARNING: Tweening the position of the trigger element affects the scene timing and should be avoided!"),!1})}if(parseFloat(TweenLite.version)>=1.14)for(var g,d,f=e.getChildren?e.getChildren(!0,!0,!1):[e],w=function(){r(2,"WARNING: tween was overwritten by another. To learn how to avoid this issue see here: https://github.com/janpaepke/ScrollMagic/wiki/WARNING:-tween-was-overwritten-by-another")},h=0;h<f.length;h++)g=f[h],d!==w&&(d=g.vars.onOverwrite,g.vars.onOverwrite=function(){d&&d.apply(this,arguments),w.apply(this,arguments)});return r(3,"added tween"),i(),t},t.removeTween=function(n){return e&&(n&&e.progress(0).pause(),e.kill(),e=void 0,r(3,"removed tween (reset: "+(n?"true":"false")+")")),t}})});
   
