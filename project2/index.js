c379734e9bace1d74e358217a95acdb95b1ae1f5
KPW1 <oooppp24@naver.com>
2023-07-24 17:39:42 +0900

    12

--- project2/index.js
+++ project2/index.js
@@ -104,101 +104,6 @@ $(window).on('scroll', () => {
 
 
 
-// !function(e,n){"function"==typeof define&&define.amd?define(["ScrollMagic","TweenMax","TimelineMax"],n):"object"==typeof exports?(require("gsap"),n(require("scrollmagic"),TweenMax,TimelineMax)):n(e.ScrollMagic||e.jQuery&&e.jQuery.ScrollMagic,e.TweenMax||e.TweenLite,e.TimelineMax||e.TimelineLite)}(this,function(e,n,o){"use strict";var t=window.console||{},r=Function.prototype.bind.call(t.error||t.log||function(){},t);e||r("(animation.gsap) -> ERROR: The ScrollMagic main module could not be found. Please make sure it's loaded before this plugin or use an asynchronous loader like requirejs."),n||r("(animation.gsap) -> ERROR: TweenLite or TweenMax could not be found. Please make sure GSAP is loaded before ScrollMagic or use an asynchronous loader like requirejs."),e.Scene.addOption("tweenChanges",!1,function(e){return!!e}),e.Scene.extend(function(){var e,t=this,r=function(){t._log&&(Array.prototype.splice.call(arguments,1,0,"(animation.gsap)","->"),t._log.apply(this,arguments))};t.on("progress.plugin_gsap",function(){i()}),t.on("destroy.plugin_gsap",function(e){t.removeTween(e.reset)});var i=function(){if(e){var n=t.progress(),o=t.state();e.repeat&&-1===e.repeat()?"DURING"===o&&e.paused()?e.play():"DURING"===o||e.paused()||e.pause():n!=e.progress()&&(0===t.duration()?n>0?e.play():e.reverse():t.tweenChanges()&&e.tweenTo?e.tweenTo(n*e.duration()):e.progress(n).pause())}};t.setTween=function(a,s,l){var c;arguments.length>1&&(arguments.length<3&&(l=s,s=1),a=n.to(a,s,l));try{(c=o?new o({smoothChildTiming:!0}).add(a):a).pause()}catch(e){return r(1,"ERROR calling method 'setTween()': Supplied argument is not a valid TweenObject"),t}if(e&&t.removeTween(),e=c,a.repeat&&-1===a.repeat()&&(e.repeat(-1),e.yoyo(a.yoyo())),t.tweenChanges()&&!e.tweenTo&&r(2,"WARNING: tweenChanges will only work if the TimelineMax object is available for ScrollMagic."),e&&t.controller()&&t.triggerElement()&&t.loglevel()>=2){var u=n.getTweensOf(t.triggerElement()),p=t.controller().info("vertical");u.forEach(function(e,n){var o=e.vars.css||e.vars;if(p?void 0!==o.top||void 0!==o.bottom:void 0!==o.left||void 0!==o.right)return r(2,"WARNING: Tweening the position of the trigger element affects the scene timing and should be avoided!"),!1})}if(parseFloat(TweenLite.version)>=1.14)for(var g,d,f=e.getChildren?e.getChildren(!0,!0,!1):[e],w=function(){r(2,"WARNING: tween was overwritten by another. To learn how to avoid this issue see here: https://github.com/janpaepke/ScrollMagic/wiki/WARNING:-tween-was-overwritten-by-another")},h=0;h<f.length;h++)g=f[h],d!==w&&(d=g.vars.onOverwrite,g.vars.onOverwrite=function(){d&&d.apply(this,arguments),w.apply(this,arguments)});return r(3,"added tween"),i(),t},t.removeTween=function(n){return e&&(n&&e.progress(0).pause(),e.kill(),e=void 0,r(3,"removed tween (reset: "+(n?"true":"false")+")")),t}})});
-  
-
-
-// var html = document.documentElement;
-//   var body = document.body;
-
-//   var scroller = {
-//     target: document.querySelector(".scroll-container"),
-//     ease: 0.09, // <= scroll speed
-//     endY: 0,
-//     y: 0,
-//     resizeRequest: 1,
-//     scrollRequest: 0,
-//   };
-
-//   var requestId = null;
-
-//   TweenLite.set(scroller.target, {
-//     rotation: 0.01,
-//     force3D: true
-//   });
-
-//   window.addEventListener("load", onLoad);
-
-//   function onLoad() {    
-//     updateScroller();  
-//     window.focus();
-//     window.addEventListener("resize", onResize);
-//     document.addEventListener("scroll", onScroll); 
-//   }
-
-//   function updateScroller() {
-    
-//     var resized = scroller.resizeRequest > 0;
-      
-//     if (resized) {    
-//       var height = scroller.target.clientHeight;
-//       body.style.height = height + "px";
-//       scroller.resizeRequest = 0;
-//     }
-        
-//     var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0;
-
-//     scroller.endY = scrollY;
-//     scroller.y += (scrollY - scroller.y) * scroller.ease;
-
-//     if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-//       scroller.y = scrollY;
-//       scroller.scrollRequest = 0;
-//     }
-    
-//     TweenLite.set(scroller.target, { 
-//       y: -scroller.y 
-//     });
-    
-//     requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null;
-//   }
-
-//   function onScroll() {
-//     scroller.scrollRequest++;
-//     if (!requestId) {
-//       requestId = requestAnimationFrame(updateScroller);
-//     }
-//   }
-
-//   function onResize() {
-//     scroller.resizeRequest++;
-//     if (!requestId) {
-//       requestId = requestAnimationFrame(updateScroller);
-//     }
-//   } 
-
-
-//   const controller = new ScrollMagic.Controller();
-
-//   var forEach = function(array, callback, scope) {
-//     for (var i = 0; i < array.length; i++) {
-//       callback.call(scope, i, array[i]);
-//     }
-//   };
-
-//   var myNodeList = document.querySelectorAll(".img-loader");
-
-//   forEach(myNodeList, function(index, value) {
-//     const tween = TweenMax.to(value, 2, { borderTopLeftRadius:"100%", borderTopRightRadius:"100%", delay:0.3, height: 0, ease: Power2.easeOut });
-    
-//     const itemScene = new ScrollMagic.Scene({
-//       triggerElement: value,
-//       triggerHook: 0.6,
-//       reverse: false
-//     })
-//     .setTween(tween)
-//     .addTo(controller);
-//   });
 
 
   // 스크롤 매직
@@ -288,7 +193,52 @@ const test2 = document.querySelector('.page2')
       }, 1000);
     }
   })
+  $(window).on('scroll', () => {
+    const test = document.querySelector('.page5')
+    let a = test.offsetTop
+    let c = a-sh
+    if(scrollY > c) {
+      setTimeout(() => {
+        $('.page5').find('h1').find('.first-1').addClass('active')
+        $('.page5').find('h1').find('.first-2').addClass('active')
+        $('.page5').find('h1').find('.first-3').addClass('active')
+      }, 200);
+      setTimeout(() => {
+        $('.page5').find('h1').find('.second-1').addClass('active')
+        $('.page5').find('h1').find('.second-2').addClass('active')
+      }, 600);
+      setTimeout(() => {
+        $('.page5').find('h1').find('.line').addClass('active')
+      }, 1000);
+    }
+  })
+  $(window).on('scroll', () => {
+    const test = document.querySelector('.page6')
+    let a = test.offsetTop
+    let c = a-sh
+    if(scrollY > c) {
+      setTimeout(() => {
+        $('.page6').find('.first').addClass('active')
 
+      }, 200);
+      setTimeout(() => {
+        $('.page6').find('.second').addClass('active')
+      }, 300);
+      setTimeout(() => {
+        
+        $('.page6').find('.third').addClass('active')
+      }, 400);
+    }
+  })
+
+$(window).on('scroll', () => {
+  const test = document.querySelector('.page8')
+  let a = test.offsetTop
+  let b = a-sh
+  if(scrollY>b) {
+    $('.page8').find('h1').addClass('active')
+  }
+})
 
   // $(window).on("wheel", function (e) {
   //   e.preventDefault();
@@ -334,3 +284,58 @@ const test2 = document.querySelector('.page2')
 
 
 
+  $(window).on('scroll', () => {
+    const a = $('.page5').find('.video-box').offset().top
+    let as = a-sh;
+    if(scrollY>as) {
+      $('.page5').find('.video-box').addClass('active')
+    }
+  })
+
+  $(window).on('scroll', () => {
+    // console.log(scrollY)
+    const a = $('.page6').find('.icon-box').find('.icon-first').offset().top
+    const b = $('.page6').find('.icon-box').find('.icon-second').offset().top
+    let as = a-sh+120;
+    let bs = b-sh+120;
+    // console.log(as)
+    if(scrollY>as) {
+      $('.page6').find('.icon-box').find('.a1').addClass('active')
+      $('.page6').find('.icon-box').find('.a2').addClass('active')
+      $('.page6').find('.icon-box').find('.a3').addClass('active')
+    }
+    if(scrollY>bs) {
+      $('.page6').find('.icon-box').find('.a4').addClass('active')
+      $('.page6').find('.icon-box').find('.a5').addClass('active')
+      $('.page6').find('.icon-box').find('.a6').addClass('active')
+    }
+  })
+
+
+  let prevScroll = window.scrollY;
+  $(window).on('scroll', () => {
+    let currentScroll = window.scrollY;
+    // 이전 스크롤 값이 크면 true, 스크롤을 올리면 header 보임
+    if (prevScroll > currentScroll) {
+      $('.left').css({ left: -20 });
+    } else {// 이후 스크롤 값이 크면 false, 스크롤을 내리면 header 숨김
+      $('.left').css({ left: 0 });
+    }
+
+    // prev에 현재 스크롤 값 대입
+    prevScroll = currentScroll;
+
+  });
+
+
+  $(window).on('scroll', () => {
+    let a = $('.footer-last').offset().top
+    let b = a-sh+150
+    console.log(b)
+    console.log(scrollY)
+    if(scrollY> b){
+      $('.footer-last').addClass('active')
+    }else {
+      $('.footer-last').removeClass('active')
+    }
+  })
